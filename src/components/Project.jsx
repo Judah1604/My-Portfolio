834409380cabdeb32da6d0e4124885d5b86be4b2
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2023-07-05 00:57:48 +0100

    Updated files

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ const Project = ({ heading, details, tools, filename, isPortfolio, url }) => {
 
     return (
         <div className="project row">
-            <motion.div className="project-text col-md-5" initial={{ translateX: -500 }} whileInView={{ translateX: 0 }}>
+            <motion.div className="project-text col-md-5" initial={{ translateX: -200 }} whileInView={{ translateX: 0 }}>
                 <h2>{heading}</h2>
                 <p>{details}</p>
                 <ul className="tools">
@@ -13,7 +13,7 @@ const Project = ({ heading, details, tools, filename, isPortfolio, url }) => {
                 <a href={"https://github.com/Judah1604/" + heading.split(' ').join('-')} className="btn code-btn" target="_blank">View Code</a>
                 {isPortfolio ? '' : <a href={"https://judah1604.github.io/" + heading.split(' ').join('-')} className="btn red-btn" target="_blank">Visit Site</a>}
             </motion.div>
-            <motion.div className="project-img col-md-5" initial={{ translateX: 500 }} whileInView={{ translateX: 0 }} transition={{ type: 'spring', bounce: 0.4 }}>
+            <motion.div className="project-img col-md-5" initial={{ translateX: 200 }} whileInView={{ translateX: 0 }} transition={{ type: 'spring', bounce: 0.4 }}>
                 <img src={"Images/Projects/" + filename} alt={heading} />
             </motion.div>
         </div>
