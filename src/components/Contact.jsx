1139856c8cb2b8bb77b265ff5369a2d1a639ecc9
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2023-07-05 00:32:18 +0100

    Updated files

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,8 @@ const Contact = () => {
             <div className="contact" id="contact">
                 <h1 className="header">Contact</h1>
                 <p>Feel free to contact me.</p>
-                <form method="post" name="Portfolio Contact Form" netlify>
+                <form method="post" name="Portfolio Contact Form" data-netlify="true">
+                    <input type="hidden" name="form-name" value="Portfolio Contact Form" />
                     <motion.div className="form-group" initial={{ translateX: -400 }} whileInView={{ translateX: 0 }}>
                         <input type="text" id="fullname" name="Full Name" value={fullname} onChange={(e) => setFullname(e.target.value)} required />
                         <label htmlFor="fullname" className={fullname === '' ? '' : 'focus-label'}>Full Name</label>
@@ -27,7 +28,7 @@ const Contact = () => {
                     <motion.div className="form-group" initial={{ translateX: 400 }} whileInView={{ translateX: 0 }}>
                         <textarea placeholder="Message" name="Message"></textarea>
                     </motion.div>
-                    <button className="btn red-btn">Send</button>
+                    <button className="btn red-btn" type="submit">Send</button>
                 </form>
             </div>
 
