fbfd1b218a46c678093d39ff908424a63b9bce8c
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2023-09-18 21:05:39 +0100

    Added Word-Count Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,83 +5,95 @@ const Projects = () => {
         <div className="projects" id="projects">
             <h1 className="header">Projects</h1>
             <div className="projects-container">
-                 <Project
-                    heading='Expense Tracker Web App'
-                    details='This is a Expense Tracker with CRUD(Create, Read, Update, Delete) capabilities, 
-                        All you have to do is to write the expense in the expense input and the price and the app creates a new item and calculates the total.
-                        You can also edit the items or delete them. Built this with:'
-                    tools={['HTML', 'SASS', 'React']}
-                    filename='expense.webp'
+                <Project
+                    heading="Word Count"
+                    details="This is a word count app that counts the number of words and characters in it. Built this with:"
+                    tools={["HTML", "SASS", "React"]}
+                    filename="word-count.webp"
                 />
                 <Project
-                    heading='My Portfolio'
-                    details='This is my portfolio site done with React. Built this with:'
-                    tools={['HTML', 'SASS', 'Bootstrap', 'React', 'Framer Motion']}
-                    filename='portfolio.PNG'
+                    heading="Expense Tracker Web App"
+                    details="This is a Expense Tracker with CRUD(Create, Read, Update, Delete) capabilities, 
+                        All you have to do is to write the expense in the expense input and the price and the app creates a new item and calculates the total.
+                        You can also edit the items or delete them. Built this with:"
+                    tools={["HTML", "SASS", "React"]}
+                    filename="expense.webp"
+                />
+                <Project
+                    heading="My Portfolio"
+                    details="This is my portfolio site done with React. Built this with:"
+                    tools={[
+                        "HTML",
+                        "SASS",
+                        "Bootstrap",
+                        "React",
+                        "Framer Motion",
+                    ]}
+                    filename="portfolio.PNG"
                     isPortfolio
                 />
                 <Project
-                    heading='Github Users Search'
-                    details='This is my first project done with React. It fetches the data of a github user,
-                        all you need to do is to write the username in the input box. Built this with:'
-                    tools={['HTML', 'CSS', 'React']}
-                    filename='ghusers.webp'
-                    url='https://judah1604.github.io/Github-user-search/'
+                    heading="Github Users Search"
+                    details="This is my first project done with React. It fetches the data of a github user,
+                        all you need to do is to write the username in the input box. Built this with:"
+                    tools={["HTML", "CSS", "React"]}
+                    filename="ghusers.webp"
+                    url="https://judah1604.github.io/Github-user-search/"
                 />
                 <Project
-                    heading='Insure Landing Page'
-                    details='This is a Frontend Mentor challenge. Built this landing page with:'
-                    tools={['HTML', 'SASS', 'JavaScript']}
-                    filename='insure.webp'
+                    heading="Insure Landing Page"
+                    details="This is a Frontend Mentor challenge. Built this landing page with:"
+                    tools={["HTML", "SASS", "JavaScript"]}
+                    filename="insure.webp"
                 />
                 <Project
-                    heading='Blogr Landing Page'
-                    details='This is a Frontend Mentor challenge. Built this landing page with:'
-                    tools={['HTML', 'SASS', 'JavaScript']}
-                    filename='blogr.webp'
+                    heading="Blogr Landing Page"
+                    details="This is a Frontend Mentor challenge. Built this landing page with:"
+                    tools={["HTML", "SASS", "JavaScript"]}
+                    filename="blogr.webp"
                 />
                 <Project
-                    heading='Calculator'
-                    details='I made this fully responsive calculator'
-                    tools={['HTML', 'SASS', 'JavaScript']}
-                    filename='calculator.webp'
+                    heading="Calculator"
+                    details="I made this fully responsive calculator"
+                    tools={["HTML", "SASS", "JavaScript"]}
+                    filename="calculator.webp"
                 />
                 <Project
-                    heading='Real Estate Site'
-                    details='This is a real estate site which I call LIVAN.
-						Worked with SVG waves when building this.'
-                    tools={['HTML', 'SASS', 'JavaScript']}
-                    filename='livan.webp'
+                    heading="Real Estate Site"
+                    details="This is a real estate site which I call LIVAN.
+						Worked with SVG waves when building this."
+                    tools={["HTML", "SASS", "JavaScript"]}
+                    filename="livan.webp"
                 />
                 <Project
-                    heading='Virtual Keyboard'
+                    heading="Virtual Keyboard"
                     details="This responsive virtual keyboard' s functionality was made by implementing DOM Manipulation."
-                    tools={['HTML', 'CSS', 'JavaScript']}
-                    filename='keyboard.webp'
+                    tools={["HTML", "CSS", "JavaScript"]}
+                    filename="keyboard.webp"
                 />
                 <Project
-                    heading='Google Clone'
+                    heading="Google Clone"
                     details="I made this clone of Google and added a Dark mode switch."
-                    tools={['HTML', 'CSS', 'JavaScript']}
-                    filename='google.webp'
+                    tools={["HTML", "CSS", "JavaScript"]}
+                    filename="google.webp"
                 />
                 <Project
-                    heading='GAZU'
+                    heading="GAZU"
                     details="I made this Resturant Site which I call GAZU.
 						Where I implemented CSS animations"
-                    tools={['HTML', 'SASS', 'JavaScript', 'Bootstrap']}
-                    filename='gazu.webp'
+                    tools={["HTML", "SASS", "JavaScript", "Bootstrap"]}
+                    filename="gazu.webp"
                 />
                 <Project
-                    heading='Selit'
+                    heading="Selit"
                     details="I made this E-Commerce Site added Add To Cart functionality.
 						I call the site Selit"
-                    tools={['HTML', 'SASS', 'JavaScript', 'Bootstrap']}
-                    filename='selit.webp'
+                    tools={["HTML", "SASS", "JavaScript", "Bootstrap"]}
+                    filename="selit.webp"
                 />
             </div>
         </div>
-    )
+    );
 }
 
 export default Projects
