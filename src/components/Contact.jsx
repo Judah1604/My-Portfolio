5b533c3e16eac204ba569b51666d65cda1d2a243
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2023-11-09 02:15:25 +0100

    Added animations to socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 
+const socialsVariants = {
+    initial: {
+        y: 40
+    },
+    animate: (delay) => ({
+        y: 0,
+        transition: {
+            delay: delay
+        }
+    })
+}
+
 const Contact = () => {
     const [fullname, setFullname] = useState('')
     const [email, setEmail] = useState('')
@@ -11,34 +23,127 @@ const Contact = () => {
             <div className="contact" id="contact">
                 <h1 className="header">Contact</h1>
                 <p>Feel free to contact me.</p>
-                <form action="/success.html" method="POST" name="Contact Form" netlify>
-                    <input type="hidden" name="form-name" value="Contact Form" />
-                    <motion.div className="form-group" initial={{ translateX: -200 }} whileInView={{ translateX: 0 }} viewport={{once: true}}>
-                        <input type="text" id="fullname" name="Full Name" value={fullname} onChange={(e) => setFullname(e.target.value)} required />
-                        <label htmlFor="fullname" className={fullname === '' ? '' : 'focus-label'}>Full Name</label>
+                <form action="#" method="POST" name="Contact Form" netlify>
+                    <input
+                        type="hidden"
+                        name="form-name"
+                        value="Contact Form"
+                    />
+                    <motion.div
+                        className="form-group"
+                        initial={{ translateX: -200 }}
+                        whileInView={{ translateX: 0 }}
+                        viewport={{ once: true }}
+                    >
+                        <input
+                            type="text"
+                            id="fullname"
+                            name="Full Name"
+                            value={fullname}
+                            onChange={(e) => setFullname(e.target.value)}
+                            required
+                        />
+                        <label
+                            htmlFor="fullname"
+                            className={fullname === "" ? "" : "focus-label"}
+                        >
+                            Full Name
+                        </label>
                     </motion.div>
-                    <motion.div className="form-group" initial={{ translateX: 200 }} whileInView={{ translateX: 0 }} viewport={{once: true}}>
-                        <input type="email" id="email" name="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                        <label htmlFor="email" className={email === '' ? '' : 'focus-label'}>Email</label>
+                    <motion.div
+                        className="form-group"
+                        initial={{ translateX: 200 }}
+                        whileInView={{ translateX: 0 }}
+                        viewport={{ once: true }}
+                    >
+                        <input
+                            type="email"
+                            id="email"
+                            name="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <label
+                            htmlFor="email"
+                            className={email === "" ? "" : "focus-label"}
+                        >
+                            Email
+                        </label>
                     </motion.div>
-                    <motion.div className="form-group" initial={{ translateX: -200 }} whileInView={{ translateX: 0 }} viewport={{once: true}}>
-                        <input type="number" id="phone" name="Phone Number" value={number} onChange={(e) => setNumber(e.target.value)} required />
-                        <label htmlFor="phone" className={number === '' ? '' : 'focus-label'}>Phone Number</label>
+                    <motion.div
+                        className="form-group"
+                        initial={{ translateX: -200 }}
+                        whileInView={{ translateX: 0 }}
+                        viewport={{ once: true }}
+                    >
+                        <input
+                            type="number"
+                            id="phone"
+                            name="Phone Number"
+                            value={number}
+                            onChange={(e) => setNumber(e.target.value)}
+                            required
+                        />
+                        <label
+                            htmlFor="phone"
+                            className={number === "" ? "" : "focus-label"}
+                        >
+                            Phone Number
+                        </label>
                     </motion.div>
-                    <motion.div className="form-group" initial={{ translateX: 200 }} whileInView={{ translateX: 0 }} viewport={{once: true}}>
-                        <textarea placeholder="Message" name="Message"></textarea>
+                    <motion.div
+                        className="form-group"
+                        initial={{ translateX: 200 }}
+                        whileInView={{ translateX: 0 }}
+                        viewport={{ once: true }}
+                    >
+                        <textarea
+                            placeholder="Message"
+                            name="Message"
+                        ></textarea>
                     </motion.div>
-                    <button className="btn red-btn" type="submit">Send</button>
+                    <button className="btn red-btn" type="submit">
+                        <i className="fa-solid fa-paper-plane"></i>
+                        Shoot
+                    </button>
                 </form>
             </div>
 
             <div className="socials">
-                <a href="https://twitter.com/JudahOyedele"><i className="fa-brands fa-twitter"></i></a>
-                <a href="https://codepen.io/Judah1604"><i className="fa-brands fa-codepen"></i></a>
-                <a href="https://github.com/Judah1604"><i className="fa-brands fa-github"></i></a>
+                <motion.a
+                    variants={socialsVariants}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0}
+                    viewport={{ once: true }}
+                    href="https://twitter.com/JudahOyedele"
+                >
+                    <i className="fa-brands fa-twitter"></i>
+                </motion.a>
+                <motion.a
+                    variants={socialsVariants}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.2}
+                    viewport={{ once: true }}
+                    href="https://codepen.io/Judah1604"
+                >
+                    <i className="fa-brands fa-codepen"></i>
+                </motion.a>
+                <motion.a
+                    variants={socialsVariants}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={0.4}
+                    viewport={{ once: true }}
+                    href="https://github.com/Judah1604"
+                >
+                    <i className="fa-brands fa-github"></i>
+                </motion.a>
             </div>
         </>
-    )
+    );
 }
 
 export default Contact
